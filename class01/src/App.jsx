6d60a8739d706cd87a7285f4602f56dd47292852
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2026-01-05 22:28:56 +0530

    Making the minue components  its importing to main.jsx and unified css applied

---
+++ class01/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Nav/>
+      <Footer/>
+    </div>
+  )
+}
+export default App
---
+++ class01/src/components/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='nav' >This is Nav</div>
+  )
+}
+
+export default Nav
