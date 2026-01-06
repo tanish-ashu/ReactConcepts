8b0e75398995395f795e679f109c24865d885266
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2026-01-07 02:45:33 +0530

    Understanding components and breaking components and building

---
+++ class1-task/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Section from './components/Section'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Section />
+    </div>
+  )
+}
+
+export default App
---
+++ class1-task/src/components/Center.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Center = () => {
+  return (
+    <div className='center' >
+      This is center
+    </div>
+  )
+}
+
+export default Center
