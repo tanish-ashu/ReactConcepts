27f2ea45d5b16a4bb65672a25abeef27743b2e3b
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2026-01-07 02:47:05 +0530

    beginning and undertanding the working and use of tailwind css

---
+++ class02/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-9xl bg-amber-300  ' >Hello tailiwind has been imported and used by index.css</h1>
+      <div className="min-h-screen flex items-center justify-center bg-blue-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Tailwind CSS is working!
+      </h1>
+    </div>
+    </div>
+  )
+}
+
+export default App
