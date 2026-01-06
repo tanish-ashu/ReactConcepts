f608b105279ce3d31c37e27ac035cb3239bb1dd5
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2026-01-07 02:43:48 +0530

    Undertanding react and its file structure

---
+++ class0/src/App.jsx
@@ -0,0 +1,6 @@
+
+const App = () => {
+  return <h1>hello from app child of main</h1>
+}
+
+export default App
---
+++ class0/src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css' // css importing
+import App from './App.jsx' // child
+i
+
+createRoot(document.getElementById('root')).render(
+  <App/>
+)
