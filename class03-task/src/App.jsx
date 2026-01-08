2cdbcc10b7ef8e0e1e6ddd44dff341d011abd5f3
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2026-01-09 02:11:50 +0530

    task for component building , reuse , and props using different data and same modified component

---
+++ class03-task/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return ( 
+    <div className = 'bg-black text-while h-screen ' >
+      
+    </div>
+  )
+}
+
+export default App
+// generate data , array of object with multiple key values and use it to make list of card using the samw kind of single card , just optimizing it for different.
