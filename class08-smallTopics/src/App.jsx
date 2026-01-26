a34b2f6bf091a73ef681c62f77fecf12279512af
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2026-01-26 20:45:33 +0530

    some concepts left  like , fragement

---
+++ class08-smallTopics/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+// THE FRAGMENT AND 
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello page</h1>
+      <h2>this is just an empty tag, 'the fragement'..</h2>
+      
+    </>
+  )
+}
+
+export default App
